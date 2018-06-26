40e7225c3a94b0ad19e883ac21f4eab6aba85596
cedric.bingen <cedric.bingen@OntCur-0E180363.vdabheverlee.intra>
2018-06-26 08:24:27 +0200

    with better URI integration

--- src/main/java/be/vdab/restclients/OpenWeatherWeerClient.java
+++ src/main/java/be/vdab/restclients/OpenWeatherWeerClient.java
@@ -1,8 +1,6 @@
 package be.vdab.restclients;
 
 import java.math.BigDecimal;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,24 +14,20 @@ import be.vdab.exceptions.KanWeerNietLezenException;
 @Component
 public class OpenWeatherWeerClient implements WeerClient {
 	private final static Logger LOGGER = Logger.getLogger(OpenWeatherWeerClient.class.getName());
-	private final URI beginOpenWeatherURL;
-	private final URI eindeOpenWeatherURL;
+	private final String openWeatherURL;
 	private final RestTemplate restTemplate;
 
-	OpenWeatherWeerClient(@Value("${beginWeerURL}") URI beginOpenWeatherURL, @Value("${eindeWeerURL}") URI eindeOpenWeatherURL, RestTemplate restTemplate) {
-		this.beginOpenWeatherURL = beginOpenWeatherURL;
-		this.eindeOpenWeatherURL = eindeOpenWeatherURL;
+	OpenWeatherWeerClient(@Value("${openWeatherURL}") String openWeatherURL, RestTemplate restTemplate) {
+		this.openWeatherURL = openWeatherURL;
 		this.restTemplate = restTemplate;
 	}
 
 	@Override
 	public BigDecimal getStadsTemperatuur(String stad) {
 		try {
-			URI openWeatherURI = new URI(beginOpenWeatherURL + stad + eindeOpenWeatherURL);
-			System.out.println("uri: " + openWeatherURI);
-			Current current = restTemplate.getForObject(openWeatherURI, Current.class);
+			Current current = restTemplate.getForObject(openWeatherURL, Current.class, stad);
 			return current.getTemperature().getValue();
-		} catch (RestClientException | URISyntaxException ex) {
+		} catch (RestClientException ex) {
 			LOGGER.log(Level.SEVERE, "kan weer niet lezen", ex);
 			throw new KanWeerNietLezenException();
 		}
